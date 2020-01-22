c9194d5b0a0b516af792535423f40bc7284744bf
Hewerson <hewerson.freitas@gmail.com>
2020-01-21 23:54:02 -0300

    Migrate and SQLAlchemy

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'concat-list.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class Production(Config):
+    DEBUG = False
+
+class Development(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    
