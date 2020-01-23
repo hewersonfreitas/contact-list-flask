a1c617232e25d67a3e0a87372ca2bf6c575840e1
Hewerson Freitas <hewerson.freitas@gmail.com>
2020-01-23 17:28:00 -0300

    Vai

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,4 @@ migrate = Migrate(app, db)
 
 
 from app import routes, models
+
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
 from app import app
+from flask import render_template, request
+
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index',methods = ['POST','GET'])
 def index():
-    return "Home Page"
+    return render_template("form.html")
+
+
+@app.route('/register',methods = ['POST','GET'])
+def register():
+    if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
