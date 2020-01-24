945ed228f2f9a457165b094a4245af47b69fdf2d
Hewerson <hewerson.freitas@gmail.com>
2020-01-24 00:27:43 -0300

    finalizado

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,27 @@
-from app import app
+from app import app, db
+from app.models import User
 from flask import render_template, request
 
 
-
 @app.route('/')
-@app.route('/index',methods = ['POST','GET'])
+@app.route('/index', methods=['POST', 'GET'])
 def index():
-    return render_template("form.html")
+    list_rows = User.query.all()
+    return render_template("form_list.html", list_rows=list_rows)
 
 
-@app.route('/register',methods = ['POST','GET'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
-      result = request.form
-      return render_template("result.html",result = result)
+        result = request.form
+        if result:
+            user = User(
+                name=result['name'],
+                telephone=result['telephone'],
+                email=result['email']
+            )
+            db.session.add(user)
+            db.session.commit()
+
+    list_rows = User.query.all()
+    return render_template("form_list.html", list_rows=list_rows)
