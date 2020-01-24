2b9afa903a8e9d286ad473760a7dbcfca7491923
Hewerson Freitas <hewerson.freitas@gmail.com>
2020-01-24 14:20:26 -0300

    Ajuste de redirect

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,9 @@
 from app import app, db
 from app.models import User
-from flask import render_template, request
+from flask import redirect, url_for, render_template, request
 
 
 @app.route('/')
-@app.route('/index', methods=['POST', 'GET'])
 def index():
     list_rows = User.query.all()
     return render_template("form_list.html", list_rows=list_rows)
@@ -23,5 +22,4 @@ def register():
             db.session.add(user)
             db.session.commit()
 
-    list_rows = User.query.all()
-    return render_template("form_list.html", list_rows=list_rows)
+    return redirect(url_for('index'))
